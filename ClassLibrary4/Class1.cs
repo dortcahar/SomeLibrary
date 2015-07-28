a012127f31f7bf8c7a06520bc76126576e5aa1d7
Üçer Aksu <uceraksu@gmail.com>
2015-07-28 05:02:28 +0300

    Second

--- ClassLibrary4/Class1.cs
+++ ClassLibrary4/Class1.cs
@@ -9,5 +9,7 @@ namespace ClassLibrary4
     public class Class1
     {
         public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
     }
 }
